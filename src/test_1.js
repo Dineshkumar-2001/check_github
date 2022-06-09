dd46a0b71737abcd683a41c658a2decd277ca149
Dineshkumar-2001 <dineshkumar@valorpaytech.com>
2022-06-09 15:49:39 +0530

    scuess

---
+++ src/test_1.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class test_1 extends Component {
+  render() {
+    return (
+      <div>
+          
+          <h1>good1</h1>
+          <h1>goxsssod1</h1>
+          <h1>goofffdfd1</h1>
+          <h1>good1</h1>
+
+          </div>
+    )
+  }
+}
